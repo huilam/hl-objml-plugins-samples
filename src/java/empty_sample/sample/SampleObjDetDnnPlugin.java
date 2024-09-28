7042ae810c7e788c9fe04dddf4dd3a14a648df15
onghuilam <onghuilam@gmail.com>
2024-09-28 15:14:49 +0800

    update to latest framework jar

--- src/java/empty_sample/sample/SampleObjDetDnnPlugin.java
+++ src/java/empty_sample/sample/SampleObjDetDnnPlugin.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import org.json.JSONObject;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.dnn.Dnn;
 
 import hl.objml2.plugin.ObjDetDnnBasePlugin;
 
@@ -16,7 +18,12 @@ public class SampleObjDetDnnPlugin extends ObjDetDnnBasePlugin {
 	@Override
 	public List<Mat> doInference(Mat aMatInput, JSONObject aCustomThresholdJson)
 	{
+		System.out.println("doInference()");
+		Mat matDnnInput = Dnn.blobFromImage(
+				aMatInput, 1.0/255.0, DEF_INPUT_SIZE, Scalar.all(0), true, false);
+		NET_DNN.setInput(matDnnInput);
 		List<Mat> listOutput = new ArrayList<>();
+		NET_DNN.forward(listOutput, NET_DNN.getUnconnectedOutLayersNames());
 		return listOutput;
 	}
 	
@@ -25,7 +32,13 @@ public class SampleObjDetDnnPlugin extends ObjDetDnnBasePlugin {
 			List<Mat> aInferenceOutputMat, 
 			Mat aMatInput, JSONObject aCustomThresholdJson)
 	{
+		System.out.println("parseDetections()");
 		Map<String, Object> mapResult = new HashMap<String, Object>();
+		for(int i=0; i<aInferenceOutputMat.size(); i++)
+		{
+			System.out.println(i+" = "+aInferenceOutputMat.get(i));
+		}
+		mapResult.put(ObjDetDnnBasePlugin._KEY_OUTPUT_TOTAL_COUNT, mapResult.size());
 		return mapResult;
 	}
 	
