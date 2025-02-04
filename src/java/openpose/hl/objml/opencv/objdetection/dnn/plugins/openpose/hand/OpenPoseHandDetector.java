1f3580400b60a898e953438c54fbd57e7a29dd3f
onghuilam <onghuilam@gmail.com>
2025-02-04 23:28:29 +0800

    initial commit for pose decoding

--- src/java/openpose/hl/objml/opencv/objdetection/dnn/plugins/openpose/hand/OpenPoseHandDetector.java
+++ src/java/openpose/hl/objml/opencv/objdetection/dnn/plugins/openpose/hand/OpenPoseHandDetector.java
@@ -2,6 +2,8 @@ package hl.objml.opencv.objdetection.dnn.plugins.openpose.hand;
 
 
 import java.util.List;
+
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import hl.objml.opencv.objdetection.dnn.plugins.openpose.BaseOpenPoseDetector;
 import hl.objml2.common.DetectedObj;
@@ -22,24 +24,35 @@ public class OpenPoseHandDetector extends BaseOpenPoseDetector {
 	    
 
 		//1*22*46*46*CV_32FC1
-		System.out.println("matResult="+matResult);
 		
-		Mat output = matResult;
+    	int iKP = super.OBJ_CLASSESS.size();
 		
-		int H = output.size(2);
-        int W = output.size(3);
-        int nPoints = super.OBJ_CLASSESS.size();
+		 // Reshape to make sure we have [78, 46, 46]
+	    Mat reshapedMat = matResult.reshape(1, new int[]{matResult.size(1), matResult.size(2), matResult.size(3)});
 		
+		int iH = reshapedMat.size(1);
+       int iW = reshapedMat.size(2);
 		
-		System.out.println("H="+H);
-		System.out.println("W="+W);
-		System.out.println("Keypoints="+nPoints);
-		
-		/**
-        
-        Pending decoding logic
+		double scaleX = (double) aMatInput.width() / iW;
+	    double scaleY = (double) aMatInput.height() / iH;
+	    
+		for (int i = 0; i < iKP; i++) {
+			// Extract heatmap for keypoint 'i'
+		    Mat heatMap = reshapedMat.row(i);
+		    heatMap = heatMap.reshape(1, iH);  // Reshape to 2D (46x46)
+		    
+	        // Find the location of the maximum confidence
+	        Core.MinMaxLocResult mmr = Core.minMaxLoc(heatMap);
+	        double confidence = mmr.maxVal;
+	        
+	        if (confidence > aConfidenceThreshold) {
+	            int xPos = (int) (mmr.maxLoc.x * scaleX);
+	            int yPos = (int) (mmr.maxLoc.y * scaleY);
 
-		**/
+	            // Add the detected keypoint with confidence score
+	            System.out.println(super.OBJ_CLASSESS.get(i)+" "+confidence+" -> ("+xPos+","+yPos+")");
+	        }
+		}
 		
 	}
 }
