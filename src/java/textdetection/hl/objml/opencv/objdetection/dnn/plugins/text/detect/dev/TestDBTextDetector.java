a793b663faf10adeb9281c5593d8b99375ac4e1f
onghuilam <onghuilam@gmail.com>
2025-10-25 00:56:46 +0800

    refactor text detection

---
+++ src/java/textdetection/hl/objml/opencv/objdetection/dnn/plugins/text/detect/dev/TestDBTextDetector.java
@@ -0,0 +1,16 @@
+package hl.objml.opencv.objdetection.dnn.plugins.text.detect.dev;
+
+import java.io.File;
+
+import hl.objml.opencv.objdetection.dnn.plugins.text.detect.DBTextDetector;
+import hl.objml2.plugin.test.BaseTester;
+
+public class TestDBTextDetector extends BaseTester {
+
+	public static void main(String[] args)
+	{
+		BaseTester tester = new BaseTester();
+		tester.setTestImageFolder(new File("./test/images/text").getAbsolutePath());
+		tester.testDetector(new DBTextDetector());
+	}
+}
